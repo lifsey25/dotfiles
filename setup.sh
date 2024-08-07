b87a1ba74efdd845a2615ff5a5e39c70fbd4db9f
Jamison <jamisonlifsey@gmail.com>
2024-08-07 19:56:32 -0400

    check for and install yay

--- setup.sh
+++ setup.sh
@@ -4,6 +4,16 @@
 
 DOTFILES_DIR="~/dotfiles" # must clone in user's home directory or edit variable
 
+install_yay() {
+  if ! command -v yay &> /dev/null; then
+    echo "yay is not installed. installing..."
+    sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+    echo "installation complete"
+  else
+    echo "yay is already installed"
+  fi
+}
+
 backup() {
   mv ~/.bashrc ~/.bashrc.bak
 
