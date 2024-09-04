f653ca4655a74ab8c35d5bad430c16550719f89e
Jamison <jamisonlifsey@gmail.com>
2024-09-04 17:14:40 -0400

    fix variable

--- addconfig.sh
+++ addconfig.sh
@@ -3,7 +3,7 @@
 
 # chmod u+x to make executable
 
-DOTFILES_DIR="~/dotfiles" # must clone in user's home directory or edit variable
+DOTFILES_DIR="$PWD" # must clone in user's home directory or edit variable
 
 backup() {
   mv ~/.bashrc ~/.bashrc.bak
